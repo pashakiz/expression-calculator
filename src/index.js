81330409d522b8708a8e4df5613f77a290665e60
pashakiz <pashakiz@gmail.com>
2021-08-30 20:52:04 +0300

    feat: implement solution by Function consctructor first

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+const expressionCalculator = (expr) => {
+  let bracketOpen = 0;
+  let bracketClose = 0;
+
+  if (expr.indexOf('/ 0') + 1) throw Error('TypeError: Division by zero.');
+
+  for(let i = 0; i < expr.length; i++) {
+      if (expr[i] === '(') bracketOpen++;
+      if (expr[i] === ')') bracketClose++;
+  }
+  if (bracketOpen !== bracketClose) throw Error('ExpressionError: Brackets must be paired.');
+  return new Function (`return ${expr}`)();
 }
 
 module.exports = {
