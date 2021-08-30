0f9bc1c67fc03dfd13b25dcd0e922f4ca4bfe1f7
pashakiz <pashakiz@gmail.com>
2021-08-30 22:57:12 +0300

    refactor: fix syntax

--- src/index.js
+++ src/index.js
@@ -4,17 +4,24 @@ function eval() {
 }
 
 const expressionCalculator = (expr) => {
-  let bracketOpen = 0;
-  let bracketClose = 0;
+  /*
+    1) убрать лишние пробеллы из expr
+        let exprArr = expr.trim().split(' ')
 
-  if (expr.indexOf('/ 0') + 1) throw Error('TypeError: Division by zero.');
+    2)
+  */
+
+  let bracketOpen = 0
+  let bracketClose = 0
+
+  if (expr.indexOf('/ 0') + 1) throw Error('TypeError: Division by zero.')
 
   for(let i = 0; i < expr.length; i++) {
-      if (expr[i] === '(') bracketOpen++;
-      if (expr[i] === ')') bracketClose++;
+      if (expr[i] === '(') bracketOpen++
+      if (expr[i] === ')') bracketClose++
   }
-  if (bracketOpen !== bracketClose) throw Error('ExpressionError: Brackets must be paired.');
-  return new Function (`return ${expr}`)();
+  if (bracketOpen !== bracketClose) throw Error('ExpressionError: Brackets must be paired.')
+  return new Function (`return ${expr}`)()
 }
 
 module.exports = {
